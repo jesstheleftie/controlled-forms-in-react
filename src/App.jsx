a73e6d3d0232d8814ffeee4a1864eaf3da3402c1
jesstheleftie <jessetang57@hotmail.com>
2024-10-31 10:18:43 -0700

    lecture notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
 // src/App.jsx
 
+// import the useState() hook
+import { useState } from "react";
+
 const App = () => {
+  // add placeholder text for the title
+  // we'll use the form to update this state by the end of this lesson
+  const [title, setTitle] = useState("The full name will appear here.");
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+  });
 
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTitle(`Your name is:${formData.firstName} ${formData.lastName}`);
+    //clears form after submitting
+    setFormData({ firstName: "", lastName: "" });
+  };
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h2>{title}</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name: </label>
+        <input
+          id="firstName"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+        <label htmlFor="lastName">Last Name: </label>
+        <input
+          id="lastName"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit Your Name</button>
+      </form>
+    </>
   );
-}
+};
 
-export default App
-
+export default App;
